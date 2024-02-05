c513acf04812aa23a37ef928b30af3334c9e4719
MyaTaheri <91988696+MyaTaheri@users.noreply.github.com>
2024-02-05 12:00:48 -0500

    2/5/2024

--- eclipse-workspace/TaheriMyaB3/src/GeorgraphyQuiz/GeographyGui.java
+++ eclipse-workspace/TaheriMyaB3/src/GeorgraphyQuiz/GeographyGui.java
@@ -3,23 +3,30 @@ package GeorgraphyQuiz;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
+import javax.print.DocFlavor.URL;
 import javax.swing.*;
 import java.awt.*;
 
 public class GeographyGui extends JFrame implements ActionListener
 {
     private File file = new File ("/Users/mt25190/Desktop/CS-HL1-Projects/eclipse-workspace/TaheriMyaB3/src/GeorgraphyQuiz/geography.txt");
-    private JTextField phraseField;
-	private JLabel resultLabel;
-	private JTextField shiftNum;
+    private JTextField stateField;
+    private JLabel questionLabel;
+	private JLabel capitalLabel;
+	private JTextField capitalField;
+    private JLabel resultLabel;
     static Scanner scnr = null;
+    private int correct = 0;
+    String state = "United States";
+    String capital = "Washington DC";
     
     public GeographyGui()
     {
@@ -31,52 +38,45 @@ public class GeographyGui extends JFrame implements ActionListener
 
 
         Color backgroundColor = new Color(215, 210, 250);
+        Color fontColor = new Color(35, 15, 135);
         getContentPane().setBackground(backgroundColor);
 
-		JLabel decryptedLabel = new JLabel("State:");
-             decryptedLabel.setBounds(40, 40, 200, 50);
-			add(decryptedLabel);
-		phraseField = new JTextField("state", 25);
-            phraseField.setBounds(250,40,150, 50);
-			add(phraseField);
-		JLabel encrypedLabel = new JLabel("Enter Capital:");
-            encrypedLabel.setBounds(40, 140, 200, 50);
-			add(encrypedLabel);
-        shiftNum = new JTextField("capital", 25);
-            shiftNum.setBounds(250,140,150, 50);
-			add(shiftNum);
-		JButton encryptButton = new JButton("Check");
-            encryptButton.setBounds(40,240,75,50);
-            encryptButton.addActionListener(this);
-			add(encryptButton);
-        JButton decryptButton = new JButton("Add Question");
-            decryptButton.setBounds(250,240,150,50);
-            decryptButton.addActionListener(this);
-            add(decryptButton);
+		questionLabel = new JLabel("What is the capital of United States");
+            questionLabel.setBounds(40, 40, 400, 50);
+            questionLabel.setForeground(fontColor);
+            questionLabel.setFont(new Font("Ink Free", Font.PLAIN, 20));
+            add(questionLabel);
+		JLabel stateLabel = new JLabel("State/Country:");
+             stateLabel.setBounds(40, 140, 200, 50);
+			add(stateLabel);
+        stateField = new JTextField("state for new question", 25);
+            stateField.setBounds(250,140,150, 50);
+			add(stateField);
+		JLabel capitalLabel = new JLabel("Enter Capital:");
+            capitalLabel.setBounds(40, 240, 200, 50);
+			add(capitalLabel);
+        capitalField = new JTextField("capital", 25);
+            capitalField.setBounds(250,240,150, 50);
+			add(capitalField);
+		JButton checkButton = new JButton("Check");
+            checkButton.setBounds(40,340,75,50);
+            checkButton.addActionListener(this);
+			add(checkButton);
+        JButton questionButton = new JButton("Add Question");
+            questionButton.setBounds(250, 340,150,50);
+            questionButton.addActionListener(this);
+            add(questionButton);
 
-		resultLabel = new JLabel("Score: X out of 50 Correct");
+		resultLabel = new JLabel("Score: XX");
 			resultLabel.setBounds(100,500,500,25);
             resultLabel.setFont(new Font("Serif", Font.PLAIN, 20));
 			add(resultLabel);
-	}
 
-
-    public void actionPerformed(ActionEvent e) 
-    {
-		if(e.getActionCommand().equals("Check"))
-		{
-            ReadLine(file);
-		}
-
-        if(e.getActionCommand().equals("Add Question"))
-		{
+        ImageIcon imageIcon = new ImageIcon("earth.gif");
+        JLabel gif = new JLabel(imageIcon);
+            gif.setBounds(150, 500, 100, 100);
+            add(gif);
             
-        }
-    }
-
-    public static String ReadLine(File file)
-    {
-        // Scanner scnr = null;
 
         try
         {
@@ -87,13 +87,61 @@ public class GeographyGui extends JFrame implements ActionListener
             System.out.println("error! - file not found");
             // System.exit(0);
         }
+        
+	}
+
+
+    public void actionPerformed(ActionEvent e) 
+    {
+		if(e.getActionCommand().equals("Check"))
+		{
+            
+            System.out.println("_____________");
+            System.out.println("Old State: " + state);
+            System.out.println("Old Capital: " + capital);
+
+
+            if (capitalField.getText().equalsIgnoreCase(capital))
+            {
+                correct++;
+            }
+            resultLabel.setText ("Score: " + correct + " correct");
+            System.out.println("Checked Score");
+                        
+            state = ReadLine(file);
+            capital = ReadLine(file);
+            
+            questionLabel.setText("What is the captital of " + state + "?");
+        
+            System.out.println("New State: " + state);
+            System.out.println("New Wanted Capital: " + capital);
+        
+		}
+
+        if(e.getActionCommand().equals("Add Question"))
+		{
+            PrintWriter outputStream = null;
+            try
+            {
+                outputStream = new PrintWriter(new FileOutputStream("geography.txt", true));
+            }
+            catch(FileNotFoundException e2)
+            {
+                System.out.println("Err 404: File Not Found");
+                System.exit(0);
+            }
+            System.out.println("Add question button");
+            outputStream.println(stateField.getText());
+            outputStream.println(capitalField.getText());
+            outputStream.close();
+        }
+    }
+
+    public static String ReadLine(File file)
+    {
        
         String line = scnr.nextLine();
-        String line2 = scnr.nextLine();
-
-        System.out.println("State: " + line);
-        System.out.println("State: " + line2);
-        scnr.close();
+        System.out.println("Read File: " + line);
         return line;
     }
     
